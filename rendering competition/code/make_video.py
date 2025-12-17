a435800036c38fa8be7c17dd6ad24cc5ff521ced
michaeltaranik <taranikmichael32@gmail.com>
2025-12-17 07:48:34 +0100

    UPDATE .gitignore, refactor the script for assemblying video

--- rendering competition/code/make_video.py
+++ rendering competition/code/make_video.py
@@ -7,10 +7,9 @@ video_name = 'video.mp4'
 images = [img for img in os.listdir(image_folder) if img.endswith(".ppm")]
 images.sort() # Ensure they are in 000, 001, 002 order
 
-images_reversed = images.copy()
-images_reversed.sort(reverse=True)
-
-images.extend(images_reversed)
+boomerang = images + images[::-1]
+num_repeats = 8
+images = num_repeats * boomerang
 # Read first image to get dimensions
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
